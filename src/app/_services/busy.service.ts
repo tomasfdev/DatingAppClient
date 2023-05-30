c0ca2619cf516bc8fb987f1731408220b820acdf
Tomás Ferreira <tomasfdev@outlook.com>
2023-05-30 15:46:41 +0100

    User edit & loading

---
+++ src/app/_services/busy.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy(){
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: "ball-pulse-sync",
+      bdColor: "rgba(255,255,255,0)",
+      size: "medium",
+      color: "#fff",
+      fullScreen: true,
+    })
+  }
+
+  idle(){
+    this.busyRequestCount--;
+    if(this.busyRequestCount <= 0){
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -2,20 +2,42 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   apiUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.apiUrl + "users");
+    if(this.members.length > 0) //se já tiver carregado members da API ent retorna esses members,
+      return of(this.members);
+
+    return this.http.get<Member[]>(this.apiUrl + "users").pipe( //se não vai buscar members,
+      map(members => {
+        this.members = members;
+        return members; //e retorna-los
+      })
+    )
   }
 
   getMemberByName(username: string){
+    const member = this.members.find(user => user.userName === username)
+    if(member) //se já tiver carregado members da API ent retorna esses members,
+      return of(member);
     return this.http.get<Member>(this.apiUrl + "users/" + username);
+  }
+
+  updateMember(member: Member){
+    return this.http.put(this.apiUrl + "users", member).pipe(
+      map(()=> {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    )
   }
 }
